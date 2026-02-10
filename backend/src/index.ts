1c9f74161f5a38ae2b5776f781503845bbfda178
soojeed <soojeed220@gmail.com>
2026-02-10 19:02:01 +0300

    add login

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,17 @@ import dotenv from 'dotenv';
 import  UserRoute from "./routes/userRoute"
 import  profileRoute  from "./routes/profileRoute"
 import  SkillsRoute  from "./routes/skillsRoute"
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: "http://localhost:5173", // React
+  credentials: true
+}));
 
 app.use('/api/user', UserRoute )
 app.use('/api/profile', profileRoute)
